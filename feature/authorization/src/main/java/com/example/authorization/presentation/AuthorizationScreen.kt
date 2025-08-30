47377e6a9ef2c48ab1e0b4cf09d2c216b631eace
henek93 <sludnikov.slava03@gmail.com>
2025-08-30 20:46:00 +0300

    добавил модуль авторизации

---
+++ feature/authorization/src/main/java/com/example/authorization/presentation/AuthorizationScreen.kt
@@ -0,0 +1,28 @@
+package com.example.authorization.presentation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AuthorizationScreen(
+    signIn: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = signIn
+        ) {
+            Text("Login")
+        }
+    }
+}
